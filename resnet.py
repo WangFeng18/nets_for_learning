2841338a5a5d0c2e81eb0f3da2d2afddcd8b641f
WangFeng18 <wangfengwork17@gmail.com>
2018-11-13 16:59:08 +0800

    c

--- resnet.py
+++ resnet.py
@@ -98,14 +98,14 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000):
+    def __init__(self, block, layers, num_classes=10):
         self.inplanes = 64
         super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        #self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
@@ -140,7 +140,7 @@ class ResNet(nn.Module):
         x = self.conv1(x) # will be 48x160
         x = self.bn1(x)
         x = self.relu(x)
-        x = self.maxpool(x)
+        #x = self.maxpool(x)
 
         x = self.layer1(x)
         self.layer1_output = x
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.backends.cudnn as cudnn
 from torchvision.datasets.cifar import CIFAR10
 import torch.utils.data.dataloader as dl
 import resnet
@@ -10,11 +11,12 @@ from tensorboardX import SummaryWriter
 import time
 import fire
 import torchvision.transforms as transforms
+from utils import progress_bar
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 class config:
     max_epoch = 400
-    lr = 0.1
+    lr = 0.001
     batch_size = 512 
     net = 'resnet18'
     num_classes = 10
@@ -36,6 +38,8 @@ train_dataloader = dl.DataLoader(CIFAR10('data/', download=True, \
 test_dataloader = dl.DataLoader(CIFAR10('data/', train=False, download=True, \
         transform=transform_test), batch_size=100, num_workers=4)
 net = getattr(resnet, config.net)(pretrained=False, num_classes=config.num_classes)
+net = nn.DataParallel(net)
+cudnn.benchmark = True
 
 def train(net, dataloader, config, load_model):
     # 1.define loss
@@ -52,41 +56,53 @@ def train(net, dataloader, config, load_model):
         scheduler.step()
         epoch_loss = 0.
         epoch_start = time.time()
+        n_total = 0.
+        n_correct = 0.
+        print('Epoch:{}'.format(i_epoch))
         for i_batch, data in enumerate(dataloader):
             input = data[0].float().cuda()
             label = data[1].long().cuda()
             optimizer.zero_grad()
             output = net(input)
             loss = criterion(output, label)
+            epoch_loss += loss.item()
+            _, predicted = output.max(1)
+            n_total += label.size(0)
+            n_correct += predicted.eq(label).sum().item()
+
             loss.backward()
             optimizer.step()
-            print('\tepoch: {:d}, batch: {:d}, loss:{:.4f}, lr:{:}'.format(i_epoch, i_batch, loss, scheduler.get_lr()))
-            epoch_loss += loss
+            progress_bar(i_batch, len(dataloader), 'L:{:.4f}|A:{:.3f}% ({}/{})'.\
+                    format(epoch_loss/(i_batch+1), n_correct/n_total*100., \
+                    int(n_correct), int(n_total)))
         epoch_end = time.time()
-        print('epoch: {:d}, epoch_loss:{:.4f}, spent:{:.4f}'.format(i_epoch, epoch_loss/i_batch, epoch_end-epoch_start))
         writer.add_scalar('epoch_loss', epoch_loss/i_batch, i_epoch)
         writer.add_scalar('epoch_time', epoch_end-epoch_start, i_epoch)
+        test(net, test_dataloader, False)
         torch.save(net.state_dict(), os.path.join('model', '{}.pth'.format(config.net)))
 
 def test(net, dataloader, load_model):
     if load_model:
         net.load_state_dict(torch.load(os.path.join('model', '{}.pth'.format(config.net))))
     net.cuda()
+    net.eval()
     n_correct = 0.
     n_total = 0.
+    epoch_loss = 0.
+    criterion = nn.CrossEntropyLoss()
     for i_batch, data in enumerate(dataloader):
         input = data[0].float().cuda()
         label = data[1].long().cuda()
         output = net(input)
+        loss = criterion(output, label)
 
+        epoch_loss += loss.item()
         _, predicted = output.max(1)
         n_total += label.size(0)
         n_correct += predicted.eq(label).sum().item()
 
-        #pred = output.argmax(1)
-        #n_correct += (pred == label).sum()
-        #n_total += data[0].size(0)
-    print n_correct/n_total
+        progress_bar(i_batch, len(dataloader), 'L:{:.4f}|A:{:.3f}% ({}/{})'.format(epoch_loss/(i_batch+1), n_correct/n_total*100., int(n_correct), int(n_total)))
+    net.train()
 
 
 
